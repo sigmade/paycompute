ead527bf229d359954d9547643e3a443fc54bc3d
sigmade <sychyov1991@mail.ru>
2020-12-20 23:09:15 +0300

    corrected

--- Paycompute/Models/EmployeeIndexViewModel.cs
+++ Paycompute/Models/EmployeeIndexViewModel.cs
@@ -7,5 +7,17 @@ namespace Paycompute.Models
 {
     public class EmployeeIndexViewModel
     {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
     }
 }
